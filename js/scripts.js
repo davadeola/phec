daf85bfc45b237f3ac7af776638c3e3b9bf270bf
David Adeola <adeoladavid15@gmail.com>
2018-05-15 13:49:52 +0300

    Phase one complete

---
+++ js/scripts.js
@@ -0,0 +1,174 @@
+var flowers = [
+  {
+    name: 'Tropical Amazon',
+    image: 'http://www.myflowerbuyer.com/v/vspfiles/photos/R-OR-TROPICAL-AMAZON-2T.jpg'
+  },
+  {
+    name: 'Mariyo',
+    image: 'http://www.flowerweb.com/media/cache/company_main/uploads/images/product/mariyosideviewnlrose.png?1372066100'
+  },
+  {
+    name:'Athena',
+    image:'https://i.pinimg.com/originals/bd/18/03/bd18038747b4756072b459b7068456a5.jpg'
+  },
+  {
+    name: 'Ice breaker',
+    image: 'https://i.pinimg.com/originals/75/cf/ee/75cfee62c6fa5b1c6d00db72378a2442.jpg'
+  },
+  {
+    name: 'Taiga',
+    image:'https://assets.bakker.com/ProductPics/810x978/73692-01-BAKI_20170518172621.jpg'
+  },
+  {
+    name:'Sodoku',
+    image:'https://www.isinyaroses.com/img/flowers/h/sudoku/3.jpg'
+  },
+  {
+    name:'Deep water',
+    image:'https://www.vanlier.co.nz/ic/3799664006/Freda%20%282%29.1.JPG'
+  },
+  {
+    name:'Tacazzi',
+    image:'http://aishaflowers.com/images/stories/virtuemart/product/Tacazzi.jpg'
+  },
+  {
+    name:'Belle Rose',
+    image:'https://www.flowerflow.com.au/wp-content/uploads/2016/02/belle-rose-2-500x500.png'
+  },
+  {
+    name:'Aqua',
+    image:'https://cdn.shopify.com/s/files/1/0497/1441/products/013_b62a3b19-e06d-4b89-8c80-e1fb40fdc60f.jpg?v=1506444205'
+  },
+  {
+    name:'Prima Ballerina',
+    image:'https://s3.eu-west-2.amazonaws.com/jparkers/1000/0001704.jpg'
+  },
+  {
+    name:'Candle light',
+    image:'https://www.tbr.co.nz/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/a/candlelight.jpg'
+  },
+  {
+    name:'Burgundy',
+    image:'https://www.fiftyflowers.com/site_files/FiftyFlowers/Image/Product/Red-Charm-Peonie-Head-500_1526947e.jpg'
+  },
+  {
+    name:'Hot Blood',
+    image:'http://www.flowersense.nl/wp-content/uploads/108393.jpg'
+  },
+  {
+    name:'Furiosa',
+    image:'https://www.flowerflow.com.au/wp-content/uploads/2016/02/furiosa-2-500x500.png'
+  },
+  {
+    name:'Madam Red',
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlC1tBKx7AUQnMQoDLlATjaJb34SGYcSmKYf1lakBIkzgQq5T'
+  },
+  {
+    name:'Good Times',
+    image:'https://www.flowerflow.com.au/wp-content/uploads/2016/02/good-times-2-500x500.png'
+  },
+  {
+    name:'Moonwalk',
+    image:'https://www.flowerflow.com.au/wp-content/uploads/2016/02/moonwalk-2-500x500.png',
+  }
+
+];
+
+var herbs=[
+  {
+    name:'Tarragon',
+    image:'https://www.wellgrowseeds.com/wp-content/uploads/2016/07/Artemisia-Dracunculus-Tarragon-500x388.jpg'
+  },
+  {
+    name:'Basil',
+    image:'https://maxpull-tlu7l6lqiu.stackpathdns.com/wp-content/uploads/2014/07/basil-400x266.jpg'
+  },
+  {
+    name:'Spear mint',
+    image:'https://cdn.shopify.com/s/files/1/1153/9196/products/Spearmintbg.jpg?v=1506398942'
+  },
+  {
+    name:'Sage',
+    image:'http://www.urbancultivator.net/wp-content/uploads/2015/10/Fresh-Sage-Leaves-1024x676.jpg'
+  },
+  {
+    name:'Dill',
+    image:'https://www.organicfacts.net/wp-content/uploads/2013/07/Dill.jpg'
+  },
+  {
+    name:'Thyme',
+    image:'https://cdn1.medicalnewstoday.com/content/images/articles/266/266016/thyme.jpg'
+  },
+  {
+    name:'Chives',
+    image:'https://i5.walmartimages.ca/images/Enlarge/164/795/6000197164795.jpg'
+  },
+  {
+    name:'Oregano',
+    image:'https://www.organicfacts.net/wp-content/uploads/2013/05/Oregano2.jpg'
+  },
+  {
+    name:'Rosemary',
+    image:'https://cdn.shopify.com/s/files/1/2211/3097/products/rosemary.jpg?v=1519021854'
+  },
+  {
+    name:'Coriander',
+    image:'https://www.organicfacts.net/wp-content/uploads/2013/05/Coriander2.jpg'
+  },
+
+]
+
+var greens=[
+    {
+      name:'Chillies',
+      image:'https://imgix.ttcdn.co/i/product/original/0/513854-c7f67bd59aa8423ea4c69e07e090e864.jpeg?q=100&auto=format%2Ccompress&w=2000'
+    },
+    {
+      name:'French Beans',
+      image:'http://raulglobalimpex.com/wp-content/uploads/2017/08/Green-Beans.png'
+    },
+    {
+      name:'Runner Beans',
+      image:'https://secure.i.telegraph.co.uk/multimedia/archive/02646/runner-beans_2646157b.jpg'
+    },
+    {
+      name:'Carrots',
+      image:'https://www.organicfacts.net/wp-content/uploads/Carrot1.jpg'
+    },
+    {
+      name:'Snow peas',
+      image:'http://www.holypine.com/wp-content/uploads/2015/10/snow-peas2.jpg'
+    },
+    {
+      name:'Okra',
+      image:'http://cdn.shopify.com/s/files/1/1380/2059/products/Okra-Summer-Beauty_grande.jpg?v=1480318424'
+    },
+    {
+      name:'Egg plants (aubergine)',
+      image:'https://cdn1.medicalnewstoday.com/content/images/articles/279/279359/eggplants.jpg'
+    },
+    {
+      name:'Sugar snap peas',
+      image:'http://fresh2door.ie/wp-content/uploads/2017/06/Fresh2Door-Sugar-Snap-Peas.jpg'
+    },
+    {
+      name:'Zucchini (courgette)',
+      image:'http://www.greefa.nl/files/Afbeeldingen%20boxen/Uw%20fruit%20en%20groenten/Courgettes_500x333pxl.jpg'
+    },
+  ];
+
+
+
+$(document).ready(function(){
+  flowers.forEach(function(flower){
+    $("#mzurie").append(' <div class="card" style="width: 15rem;"> <img class="card-img-top" src="'+flower.image+'" alt="phec images"><div class="card-body"><p class="card-text">'+flower.name+'</p></div> </div>');
+  });
+
+  herbs.forEach(function(herb){
+    $("#herbs").append(' <div class="card" style="width: 18rem;"> <img class="card-img-top" src="'+herb.image+'" alt="phec images"><div class="card-body"><p class="card-text">'+herb.name+'</p></div> </div>');
+  });
+
+  greens.forEach(function(green){
+    $("#greens").append(' <div class="card" style="width: 18rem;"> <img class="card-img-top" src="'+green.image+'" alt="phec images"><div class="card-body"><p class="card-text">'+green.name+'</p></div> </div>');
+  });
+});
