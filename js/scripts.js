902a9b2155ff972bb9c4054908d859939b009a20
David Adeola <adeoladavid15@gmail.com>
2018-07-19 17:10:13 +0300

    Added the menu bar

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,37 @@ var items = {
     orange: [
       {
         name: 'CONFIDENTIAL ',
-        image: 'https://drive.google.com/open?id=1i2sjHlKPm9QWN7gY6iTD0zp8k3-_M1-t',
+        image: '',
         description:'Colour: Orange • Bud size: 5.5cm x 5.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
         type:'Extra Premium T-Hybrid'
       },{
         name: 'AIRFORCE ',
-        image: 'https://drive.google.com/open?id=1tv8bxr46AqYS48ECQmRVcq2dkpVvkK_i',
+        image: '',
         description:'Colour: Orange • Bud size: 6.0cm x 5.0cm • Length: 50cm to 80cm • Vase life: 20 Days',
         type:'Extra Premium T-Hybrid'
       },{
         name: 'MARIYO',
-        image: 'https://drive.google.com/open?id=1t6lNujM0jqGhBS9Rq4wbNyLFjNQdyl0-',
+        image: '',
         description:'Colour: Orange • Bud size: 4.0cm x 3.5cm • Length: 50cm to 80cm • Vase life: 15 Days (variants 4.5 x 3.5 bud size, 40 to 80 cm Length; 5 x 3 bud size, 40 to 70 cm Length; 4 x 3 bud size, 35 to 70 cm length)',
         type:'Premium T-Hybrid'
       },{
         name: 'TROPICAL AMAZON',
-        image: 'https://drive.google.com/open?id=1BV3qt19tIQ8CbTO-W49ITRBoQZbAQl6_',
+        image: '',
         description:'Color: Orange • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life: 12 Days',
         type:'Intermediate'
       },{
         name: 'JULISCHKA',
-        image: 'https://drive.google.com/open?id=1l1XurKsebW7JxQ9jLXc2MXVDMn9np7Oh',
+        image: '',
         description:'Colour: Orange, Stem Length: 40-60cm, Bud Size: 5.0 x 3.8(cm), Vase life: 10 - 12 days.',
         type:'Premium Plus Rose'
       },{
         name: 'ORANGE CRUSH',
-        image: 'https://drive.google.com/open?id=1aKGmJ1st3doiuePZ-E6raxePD34abGAh',
+        image: '',
         description:'Colour: Orange, Stem Length: 40-70cm, No. of Petals: 30 – 45, Vase life: 11 days.',
         type:'Standard Rose'
       },{
         name: 'DOWNTOWN',
-        image: 'https://drive.google.com/open?id=1mWBBrld3WI8xk92pGigrztTOTXKdQ-8L',
+        image: '',
         description:'Colour: Orange, Stem Length: 40-70cm, No. of Petals: 30 - 45, Vase life: 11 days.',
         type:'Premium Rose'
       }
@@ -41,17 +41,17 @@ var items = {
     white: [
       {
         name: 'A one',
-        image: 'https://drive.google.com/open?id=1Q7eC2m7305DrWInqLLtajAVtdarwAsuD',
+        image: '',
         description:'Color: WHITE • Bud size: 6.5cm x 5.0cm • Length: 50cm to 80cm • Vase life: 20 Days',
         type:'Extra Premium T-Hybrid'
       },{
         name: 'Akito',
-        image: 'https://drive.google.com/open?id=1HTXzkAdLL8X1bqiS-BuP2wFfcpLZf9-r',
+        image: '',
         description:'Color: WHITE • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life: 12 Days',
         type:'Intermediate'
       },{
         name: 'ICE BREAKER ',
-        image: 'https://drive.google.com/open?id=10WFFessvIL-sulNyTTBx38Y7GVpd-vq0',
+        image: '',
         description:'Color: WHITE • Bud size: 6.0cm x 5.0cm • Length: 50cm to 80cm • Vase 17 Days',
         type:'Extra Premium T-Hybrid'
       },{
