c89f18e022e40235746f1b29ba742242bf7e7d40
David Adeola <adeoladavid15@gmail.com>
2018-07-20 15:49:02 +0300

    Updated the scripts.js without their respective images

--- js/scripts.js
+++ js/scripts.js
@@ -56,98 +56,419 @@ var items = {
         type:'Extra Premium T-Hybrid'
       },{
         name: 'JOMILE',
-        image: 'https://drive.google.com/open?id=1UPIexJoW61X07VyijVgJ1LVGIcUdHR-u',
+        image: '',
         description:'Color: WHITE • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life: 15 Days',
         type:'Intermediate'
       },
       {
         name: 'Athena',
-        image: 'https://drive.google.com/open?id=1i53fgLpQLpH-UHLT_Zko91x6nLOtRqu9',
+        image: '',
         description:'Color: Cream; length 40-60 cm; Bud size 4.2×3 cm (Variant: 35-60 cm in length4×3.5 for the bud size)',
         type:'Regular'
       },{
         name: 'KIWI',
-        image: 'https://drive.google.com/open?id=1JW909ljmvQt3WHPbggL9Rikw-4ri6YQE',
+        image: '',
         description:'Medium headed Rose, Color: Cream, Stem Length: 35-70cm, Vase life: 12 - 14 days.',
         type:'Regular'
       },{
         name: 'ADMISSION',
-        image: 'https://drive.google.com/open?id=1VcOGnvKkDfQUuGVDQ4wxMNebXHz50PGG',
+        image: '',
         description:'Color: Cream, Stem Length: 40-60cm, Vase life: 11 days.',
         type:'Standard Rose'
       },{
         name: 'MAGIC AVALANCHE',
-        image: 'https://drive.google.com/open?id=1PZdejn_-J8jvUbpVN5HRZbY8pWdJsSjo',
+        image: '',
         description:'Color: Peach, Stem Length: 40-70cm, Vase life: 11 days.',
         type:'Premium Rose'
       },{
         name: 'Carema',
-        image: 'https://drive.google.com/open?id=1BtAYV2O9mTgZmxJIp13bw3_a-e9CIwIO',
+        image: '',
         description:'',
         type:''
       },
     ],
     pink: [
-
+      {
+        name: 'SPLENDID CERISE ',
+        image: '',
+        description:'Colour: Cerise • Bud size: 5.5cm x 5.0cm • Length: 50cm to 80cm • Vase life: 17 Days',
+        type:'Extra Premium T-Hybrid'
+      },{
+        name: 'SWING',
+        image: '',
+        description:'Colour: Pink • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life 12 Days',
+        type:'Intermediate'
+      },{
+        name: 'CELEB ',
+        image: '',
+        description:'Colour: Pink • Bud size: 6.0cm x 5.0cm • Length: 50cm to 80cm • Vase life: 20 Days',
+        type:'Extra Premium T-Hybrid'
+      },{
+        name: 'DEEP WATER',
+        image: '',
+        description:'Colour: Purple • Bud size: 6.0cm x 5.0cm • Length: 50cm to 80cm • Vase life: 20 Days (Variant: Premium Rose Bud size 5.2 x 4.1 cm, Length 50 to 70 cm)',
+        type:'Extra Premium T-Hybrid'
+      },{
+        name: 'HIGH AND CANDY',
+        image: '',
+        description:'Colour: Pink/White • Bud size: 5.5cm x 5.0cm • Length: 50cm to 80cm • Vase life: 14 Days',
+        type:'Extra Premium T-Hybrid'
+      },{
+        name: 'HIGH AND TENDER ',
+        image: '',
+        description:'Colour: PINK • Bud size: 7.5cm x 6.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:'Extra Premium T-Hybrid'
+      },{
+        name: 'AQUA ',
+        image: '',
+        description:'Colour: Pink • Bud size: 4.5cm x 4.0cm • Length: 50cm to 80cm • Vase life: 14 Days (Variant: Premium 40-60 cm length; Bud size 4.5 x 2.5 cm)',
+        type:'T-Hybrid'
+      },{
+        name: 'MARINA',
+        image: '',
+        description:'Colour: Cerise • Bud size: 4.5cm x 4.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:'T-Hybrid'
+      },{
+        name: 'REVIVAL',
+        image: '',
+        description:'Colour: Pink • Bud size: 6.0cm x 5.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:'Premium T-Hybrid'
+      },{
+        name: 'SODUKU ',
+        image: '',
+        description:'Colour: Light Pink • Bud size: 4.5cm x 4.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:'Premium T-Hybrid'
+      },{
+        name: 'TAIGA',
+        image: '',
+        description:'Colour: Pink, Stem Length: 40-70cm, Bud size of 4 × 2.5 cm, Vase life: 11 days.',
+        type:'Regular Rose'
+      },{
+        name: 'TACAZZI+',
+        image: '',
+        description:'Colour: Pink, Bud size 4.0×3.5 cm, Stem Length: 40-70cm, Vase life: 16 days',
+        type:'Regular'
+      },{
+        name: 'BELLE ROSE',
+        image: '',
+        description:'Colour: Pink, Stem Length: 35-80cm, 5.0×3.5 in bud size, Vase life: 16 days.',
+        type:'Regular'
+      },{
+        name: 'FUCHSIANA',
+        image: '',
+        description:'',
+        type:'Regular Rose'
+      },{
+        name: 'DEKORA',
+        image: '',
+        description:'Large Headed Rose, Colour: Pink, Stem Length: 40-70cm, Vase life: 16 days.',
+        type:'Regular'
+      },{
+        name: 'LA BELLE',
+        image: '',
+        description:'Large Headed Rose, Colour: Pink, Stem Length: 40-60cm, Vase life: 16 days.',
+        type:'Regular'
+      },{
+        name: 'NEW ORLEANS',
+        image: '',
+        description:'Colour: Lilac, Stem Length: 40-60cm, Vase life: 12-14 days.',
+        type:'Regular rose'
+      },{
+        name: 'SORBET AVALANCHE',
+        image: '',
+        description:'Colour: Light Pink, Stem Length: 40-70cm, Vase life: 12-14 days.',
+        type:'Regular rose'
+      },{
+        name: 'ROSANA',
+        image: '',
+        description:'Colour: Purple, Stem Length: 40-60cm, Vase life: 12-14 days.',
+        type:'Regular rose'
+      },{
+        name: 'NIGHTINGALE',
+        image: '',
+        description:'Colour: Lilac-light Purple, Stem Length: 40-60cm, Vase life: 14 days.',
+        type:'Premium Plus Rose'
+      },{
+        name: 'PRIMA BALLERINA',
+        image: '',
+        description:'Colour: Very Light Pink, 35-50 cm stem length, and a bud size of 3.5×3.',
+        type:'Premium rose'
+      },{
+        name: 'BOOGY',
+        image: '',
+        description:'',
+        type:''
+      },{
+        name: 'WILD THING',
+        image: '',
+        description:'',
+        type:''
+      },{
+        name: 'UPPER IMPACT',
+        image: '',
+        description:'',
+        type:''
+      },{
+        name: 'MAYFAIR!',
+        image: '',
+        description:'Color: Peach, Stem Length: 40-70cm, Vase life: 12-14 days.',
+        type:'Regular Rose'
+      },
     ],
     bicolour: [
-
+      {
+        name: 'CUBA LIBRE',
+        image: '',
+        description:'Color: BICOLOR • Bud size: 5.0cm x 4.0cm • Length: 50cm to 80cm • Vase life: 13 Days',
+        type:'Premium T-Hybrid'
+      },{
+        name: 'FARFALLA ',
+        image: '',
+        description:'Color: BICOLOR • Bud size: 4.0cm x 3.5cm • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:'Premium T-Hybrid'
+      },{
+        name: 'Inka',
+        image: '',
+        description:'Color: BI COLOR • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life: 17 Days',
+        type:'Intermediate'
+      },{
+        name: 'MARIE CLAIRE!',
+        image: '',
+        description:'Color: Bi-Color Orange, Stem Length: 35-80cm, Bud Size: 4.5 x 3.5(cm), Vase life: 10 - 12 days.',
+        type:'Special Premium'
+      },{
+        name: 'ESPANA',
+        image: '',
+        description:'Colour: Bi-Colour, Stem Length: 50-70cm, Vase life: 14 days.',
+        type:'Premium plus Rose'
+      },{
+        name: 'TAMARA',
+        image: '',
+        description:'Colour: Bi-Colour, Stem Length: 40-60cm, Vase life: 12-14 days.',
+        type:'Regular Rose'
+      },{
+        name: 'CATCH',
+        image: '',
+        description:'Colour: Bi-color, Stem Length: 35-80cm, Bud Size 4×3.5cm',
+        type:'Premium Rose'
+      },{
+        name: 'CANDLE LIGHT',
+        image: '',
+        description:'Colour: Golden yellow and melted butter , Stem Length 35-80 cm, Bud size:5×3.5 cm',
+        type:'Premium rose'
+      },{
+        name: 'EYE CATCHER',
+        image: '',
+        description:'Colour: Pink bi colour • Bud size: 6.0cm x 5.0cm • Length: 50cm to 80cm • Vase life: 17 Days',
+        type:'Extra Premium T-Hybrid'
+      },{
+        name: 'LOVELY JEWEL ',
+        image: '',
+        description:'Colour: Pink Bi-colour • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life: 17 Days',
+        type:'Intermediate'
+      },{
+        name: 'NICOLETTA',
+        image: '',
+        description:'Colour: Bi-Colour, Stem Length: 40-60cm, Bud size: 4.9 x 3.0 (cm).',
+        type:'Premium plus rose'
+      },{
+        name: 'GLOW!',
+        image: '',
+        description:'Colour: Bi-Colour, Stem Length: 35-80cm, Bud size: 5.0 x 4.0 cm',
+        type:'Premium Plus Rose'
+      },{
+        name: 'H30',
+        image: '',
+        description:'',
+        type:''
+      },
     ],
     red: [
       {
           name: 'FURIOSA',
-          image: 'https://drive.google.com/open?id=1joeuGipD-xvEObpICQQrfbTu6HWMMiZs',
+          image: '',
           description:'Colour: Red, Large headed, Stem Length: 40-70cm, Vase life: 11 days.',
           type:'Premium'
         }, {
           name: 'RED RIBBON',
-          image: 'https://drive.google.com/open?id=1PaMg5TjNUr5bbsxJlWFgwiPAPmvKmqK_',
+          image: '',
           description:'Colour: Red, Large Headed Rose, Stem Length: 40-80cm, Vase life: 14 days.',
           type:'Premium'
         }, {
           name: 'BINGO CERISE',
-          image: 'https://drive.google.com/file/d/16SzdWNZ9BeDMKM5uKrbDYtKQVXhb4zfA/view',
+          image: '',
           description: 'Colour: Cerise •Bud size: 4.5cm x 4.0cm •Length:50cm to 80cm •Vase life: 12 Days',
           type:'T-Hybrid'
         },{
           name:'RED CALYPSO',
-          image:'https://drive.google.com/open?id=1OHuTuXOa44Et7aRZkE0MZfPDUOMaYXO3',
+          image:'',
           description:'Colour: Red • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life: 15 Days',
           type:'Intermediate'
         },{
           name:'UPPER CLASS',
-          image:'https://drive.google.com/open?id=1eYKkczxcMkUQV6W_dAUtFBPby1QRcd_k',
+          image:'',
           description:'Colour: Red • Bud size: 4.5cm x 4.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
           type:'Premium T- Hybrid'
         },{
           name:'RED ONE',
-          image:'https://drive.google.com/open?id=1lw6Nj1vZcZ6ntUt_HOwOFjF66_k0Q21s',
+          image:'',
           description:'Colour: Red • Bud size: 4.5cm x 4.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
           type:'T-Hybrid'
         },{
           name:'MADAM RED',
-          image:'https://drive.google.com/open?id=1E0mLckTBJmf8Ik-O8ZltXxEU2HTi_1ip',
+          image:'',
           description:'Colour: Red • Bud size: 5.5cm x 5.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
           type:'Extra Premium T-Hybrid'
         },{
           name:'HOT BLOOD',
-          image:'https://drive.google.com/open?id=1B2kdk1ePbypzRkHhIqdDnP0o9pX5TFyy',
+          image:'',
           description:'Colour: Red • Bud size: 5.5cm x 5.0cm • Length: 50cm to 80cm • Vase life: 15 Days (can also find Bud Size 4.0 x 3.5 cm Length 40 to 70 cm as a Premium Rose)',
           type:'Extra Premium T-Hybrid'
         },{
           name:'BURGANDY',
-          image:'https://drive.google.com/open?id=1dPhKwXCBQNCQpgi2xUrqjIs-JEyO__dx',
+          image:'',
           description:'Colour: Red • Bud size: 5.0cm x 4.0cm • Length: 50cm to 80cm • Vase life: 20 Days',
           type:'Premium T-Hybrid'
         }
 
     ],
     yellow: [
-
+      {
+        name: 'Radiance',
+        image: '',
+        description:'',
+        type:''
+      },{
+        name: 'INTENSE ',
+        image: '',
+        description:'Colour: Yellow • Bud size: 4.5cm x 4.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:'T-Hybrid'
+      },{
+        name: 'ROYAL SPHYNX',
+        image: '',
+        description:'Colour: Yellow • Bud size: 5.0cm x 4.0cm • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:'Premium T-Hybrid'
+      },{
+        name: 'SONRISA',
+        image: '',
+        description:'Colour: Yellow • Bud size: 4.0cm x 3.0cm • Length: 40cm to 80cm • Vase life: 12 Days (Regular Rose: 3 x 2.5cm bud size; Length 35 to 50 cm)',
+        type:'Intermediate'
+      },{
+        name: 'MOONWALK',
+        image: '',
+        description:'Colour: Yellow, Stem Length: 35-60cm, Bud size: 4.5 x 3.8 cm, Vase life: 16 days.',
+        type:'Premium'
+      },{
+        name: 'GOOD TIMES',
+        image: '',
+        description:'Colour: Yellow, Bud Size 4.2 x 3 cm, Stem Length: 35-80cm, Vase life: 16 days.',
+        type:'Premium'
+      },
     ],
     spray:[
-
-    ]
+      {
+        name: 'SONORA',
+        image: '',
+        description:'Colour: Orange, Stem Length: 40-80cm, Vase life: 14 days.',
+        type:''
+      },{
+        name: 'BABE',
+        image: '',
+        description:'Colour: Orange, Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'MIRABEL',
+        image: '',
+        description:'Colour: Red, Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'RUBICON',
+        image: '',
+        description:'Colour: Red , Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'HALEY',
+        image: '',
+        description:'Colour: Red, Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'LIANNE',
+        image: '',
+        description:'Color: Cerise • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:''
+      },{
+        name: 'NATHALIE',
+        image: '',
+        description:'Colour: Pink, Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'DINARA',
+        image: '',
+        description:'Colour: Light-Pink, Stem Length: 40-80cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'SWEET SARA',
+        image: '',
+        description:'Color: Cream White • Length: 50cm to 80cm • Vase life 12 Days',
+        type:''
+      },{
+        name: 'YELLOW BABE',
+        image: '',
+        description:'Colour: Yellow , Stem Length: 50-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'MARISA',
+        image: '',
+        description:'Colour: Yellow , Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'ABEBA',
+        image: '',
+        description:'Color: Orange • Length: 50cm to 80cm • Vase Life: 12 Days',
+        type:''
+      },{
+        name: 'SALINERO',
+        image: '',
+        description:'Colour: White, Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'SNOWFLAKES',
+        image: '',
+        description:'Color: White • Length: 50cm to 80cm • Vase life: 17 Days',
+        type:''
+      },{
+        name: 'FIREWORKS',
+        image: '',
+        description:'Color: BI COLOR • Length: 50cm to 80cm • Vase life: 15 Days',
+        type:''
+      },{
+        name: 'FIREFLASH',
+        image: '',
+        description:'Color: Bi-color • Length: 50cm to 80cm • Vase life: 12 Days',
+        type:''
+      },{
+        name: 'PINK FLASH',
+        image: '',
+        description:'Color: Pink Bi-Color • Length: 50cm to 80cm • Vase Life: 15 Days',
+        type:''
+      },{
+        name: 'REFLEX',
+        image: '',
+        description:'Colour: Pink Bi-Color, Stem Length: 40-70cm, Vase life: 12-14 days.',
+        type:''
+      },{
+        name: 'FLASH NIGHT',
+        image: '',
+        description:'Colour: Bi-Colour Red White, Stem Length: 40-70cm, Vase life: 10-12 days.',
+        type:''
+      },{
+        name: 'SUMMER DANCE',
+        image: '',
+        description:'Colour: Pink Bicolor , Stem Length: 50-70cm, Vase life: 12-14 days.',
+        type:''
+      },
+    ],
+    
   },
 };
 
